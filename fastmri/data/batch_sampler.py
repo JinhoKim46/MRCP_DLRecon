1ea8df3d1d3e110d4a7f44222a1d89dcf5e0d7ef
J.Kim <kim.jinho@siemens-healthineers.com>
2024-05-24 16:28:19 +0000

    Speed up

--- fastmri/data/batch_sampler.py
+++ fastmri/data/batch_sampler.py
@@ -6,52 +6,68 @@ Modifications and additional features by Jinho Kim are licensed under the MIT li
 as detailed in the accompanying LICENSE file.
 ===============================================================================
 """
-from fastmri.data.mri_data import SliceDataset
-from torch.utils.data import Sampler
+
 from random import shuffle
-from collections import defaultdict
+from torch.utils.data import Sampler
+from fastmri.data.mri_data import SliceDataset
 
 
 class BatchSampler(Sampler):
     """
-    This BatchSampler samples batches of indices for MRCP data, not individual samples.   
+    This BatchSampler samples batches of indices for MRCP data, not individual samples.
     This sampler does not discard last even though the last batch may be smaller than the specified batch size.
 
     """
+
     def __init__(
         self,
         dataset: SliceDataset,
         batch_size: int,
         is_train: bool = True,
     ):
-        self.dataset = [dataset[i] for i in range(len(dataset))]
+        self.dataset = [(i[0].stem, i[1]) for i in dataset.examples]
         self.batch_size = batch_size
         self.is_train = is_train
         self.batches = self.get_batches()
-        
-    def __iter__(self):        
+
+    def __iter__(self):
         return iter(self.batches)
 
     def __len__(self):
         return len(self.batches)
 
     def get_batches(self):
-        # Create a dictionary to group indices by their shapes
-        shape_groups = defaultdict(list)
-        for index, data in enumerate(self.dataset):
-            shape_groups[data.input.shape].append(index)
+        batches, batch = [], []
+        i = 0
+        batch_cnt = 0
+        cur_fname = None
+        while i < len(self.dataset):
+            if batch_cnt == self.batch_size:
+                batches.append(batch)
+                batch = []
+                cur_fname = None
+                batch_cnt = 0
 
-        # If in training mode, shuffle the groups for randomness
-        grouped_indices = list(shape_groups.values())
-        if self.is_train:            
-            for group in grouped_indices:
-                shuffle(group)
-            shuffle(grouped_indices)
+            if cur_fname is None:
+                cur_fname = self.dataset[i][0]
+                batch.append(self.dataset[i][1])
+                i += 1
+                batch_cnt += 1
+            else:
+                if self.dataset[i][1] == cur_fname:
+                    batch.append(self.dataset[i][1])
+                    i += 1
+                    batch_cnt += 1
+                else:
+                    batches.append(batch)
+                    batch = []
+                    cur_fname = None
+                    batch_cnt = 0
 
-        # Generate batches
-        batches = []
-        for group in grouped_indices:
-            for i in range(0, len(group), self.batch_size):
-                batches.append(group[i:i + self.batch_size])
-        
+        if len(batch) > 0:
+            batches.append(batch)
+
+        if self.is_train:
+            shuffle(batches)
+
         return batches
